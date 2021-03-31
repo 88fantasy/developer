7debffaedf37c08e47867c55e74692c49c0ec75b
88fantasy <88fantasy@gmail.com>
2021-03-31 15:19:48 +0800

    gateway pom fix

--- developer-gateway/src/main/java/com/gzmpc/business/developer/gateway/application/GatewayApplication.java
+++ developer-gateway/src/main/java/com/gzmpc/business/developer/gateway/application/GatewayApplication.java
@@ -3,9 +3,10 @@ package com.gzmpc.business.developer.gateway.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.tsf.circuitbreaker.config.TsfCircuitBreakerFilterAutoConfiguration;
 import org.springframework.tsf.annotation.EnableTsf;
 
-@SpringBootApplication(scanBasePackages = { "com.gzmpc.*" })
+@SpringBootApplication(scanBasePackages = { "com.gzmpc.*" }, exclude = {})
 @EnableFeignClients(basePackages = { "com.gzmpc.*" }) // 使用Feign微服务调用时请启用
 @EnableTsf
 public class GatewayApplication {
